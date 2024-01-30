1863f9ed013becfce1df58f45e3ddecba3137571
mojavadp79 <mojavadp79@gmail.com>
2024-01-30 23:58:49 +0330

    feat : create user complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
 import { MongoModule } from './databases/mongo/mongo.module';
 import { UserModule } from './modules/user/user.module';
-import { OtpService } from './services/otp/otp.service';
 
 @Module({
   imports: [AuthModule, MongoModule, UserModule],
   controllers: [],
-  providers: [OtpService],
+  providers: [],
 })
 export class AppModule {}
