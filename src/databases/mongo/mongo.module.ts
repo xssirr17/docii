cbfb61ea69af9bf34557e9b29795ffa7d86a5023
mojavadp79 <mojavadp79@gmail.com>
2024-01-30 22:37:37 +0330

    fix : mongo not connect problem

--- src/databases/mongo/mongo.module.ts
+++ src/databases/mongo/mongo.module.ts
@@ -2,7 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/docii')],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
+      dbName: 'docii',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
