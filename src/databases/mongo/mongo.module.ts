e7efbb91785e4513d3e0d408760f3c4064a45ff1
mojavadp79 <mojavadp79@gmail.com>
2024-02-02 15:44:47 +0330

    feat : docker compose

--- src/databases/mongo/mongo.module.ts
+++ src/databases/mongo/mongo.module.ts
@@ -3,8 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017', {
-      dbName: 'docii',
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.DATABASE_URI,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
     }),
   ],
   controllers: [],
